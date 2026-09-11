51497e1896e17db996abba6a864c91f978dd4026
Miguel Decode <migueldecode@gmail.com>
2026-09-11 14:21:25 +0200

    Initial LazyVim configuration

---
+++ nvim/lua/plugins/autosave.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "okuuva/auto-save.nvim",
+    opts = {
+      enabled = true,
+      trigger_events = {
+        immediate_save = { "BufLeave", "FocusLost" },
+        defer_save = { "InsertLeave", "TextChanged" },
+      },
+    },
+  },
+}
